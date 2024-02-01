b984a3ec2fcc21f86ec3ed0868c44d0d286a94b6
forensictdev <92120631+forensictdev@users.noreply.github.com>
2024-01-31 22:51:03 -0500

    Create snipSecurityMisconfiguration4.java

---
+++ codereview101/snipSecurityMisconfiguration4.java
@@ -0,0 +1,14 @@
+public class Permissions {
+    public static void main(String[] args) {
+        AccessController.doPrivileged((PrivilegedAction<Void>) () -> {
+            System.setProperty("java.security.policy", "myPolicy.policy");
+            return null;
+        });
+
+        try {
+            Class.forName("com.example.MyClass");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
