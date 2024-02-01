adea5057c0edbea00148335475309668c58ed33d
forensictdev <92120631+forensictdev@users.noreply.github.com>
2024-01-31 23:30:57 -0500

    Create snipSensitiveDataExposure1.java

---
+++ codereview101/snipSensitiveDataExposure1.java
@@ -0,0 +1,10 @@
+public class CreditCardData {
+
+    public static void main(String[] args) {
+        String creditCardNumber = "1234-5678-9012-3456";
+        double accountBalance = 5000.0;
+
+        System.out.println("The credit card number is: " + creditCardNumber);
+        System.out.println("The account balance is: $" + accountBalance);
+    }
+}
