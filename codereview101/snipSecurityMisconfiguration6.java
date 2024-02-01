19a2bd635a6fc7a5e4818b4e146969a7ab378b57
forensictdev <92120631+forensictdev@users.noreply.github.com>
2024-01-31 23:11:00 -0500

    Create snipSecurityMisconfiguration6.java

---
+++ codereview101/snipSecurityMisconfiguration6.java
@@ -0,0 +1,20 @@
+public class Connection {
+    public static void main(String[] args) {
+        try {
+            URL url = new URL("https://www.example.com");
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+            connection.setRequestMethod("GET");
+            connection.connect();
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
