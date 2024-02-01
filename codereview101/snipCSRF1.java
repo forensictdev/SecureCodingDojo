1c43b3562d48c0e56f99710cfd5ab485193324f6
forensictdev <92120631+forensictdev@users.noreply.github.com>
2024-02-01 08:51:46 -0500

    Create snipCSRF1.java

---
+++ codereview101/snipCSRF1.java
@@ -0,0 +1,12 @@
+public class ChangePasswordServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String newPassword = request.getParameter("newPassword");
+        User user = getAuthenticatedUser(); // Fictional function to get the authenticated user
+        user.setPassword(newPassword);
+        saveUser(user); // Fictional function to save the modified user
+
+        PrintWriter out = response.getWriter();
+        out.println("Password changed successfully");
+    }
+}
