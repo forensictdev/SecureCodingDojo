be1482be87aeadec7172e59c1784c21154965030
forensictdev <92120631+forensictdev@users.noreply.github.com>
2024-01-31 23:32:17 -0500

    Create snipSensitiveDataExposure3.java

---
+++ codereview101/snipSensitiveDataExposure3.java
@@ -0,0 +1,11 @@
+public class UserLogin {
+    public static void main(String[] args) {
+        String username = "myuser";
+        String password = "mypassword";
+        
+        String url = "https://example.com/login?username=" + username + "&password=" + password;
+
+        // Perform a request with the URL containing user data
+        // ...
+    }
+}
